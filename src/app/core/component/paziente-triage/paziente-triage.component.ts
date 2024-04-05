c59f42b1e4ddc40130b087d2d2859a838676db14
Ivan Robol <Ivan.Robol@Marconi.loc>
2024-04-05 16:47:34 +0200

    ulteriori modifiche

--- src/app/core/component/paziente-triage/paziente-triage.component.ts
+++ src/app/core/component/paziente-triage/paziente-triage.component.ts
@@ -4,20 +4,31 @@ import { CoreHttpService } from '../../service/coreHttp/core-http.service';
 import { AllPazienti } from '../../interface/all-pazienti';
 import { Persone } from '../../interface/persone';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RicercaPersona } from '../../interface/ricerca-persona';
 
 @Component({
   selector: 'app-paziente-triage',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './paziente-triage.component.html',
   styleUrl: './paziente-triage.component.css'
 })
 export class PazienteTriageComponent {
-  constructor(private router:Router, private coreHttp:CoreHttpService){}
   $persone: Persone[];
   stampaErrore: unknown;
+  ricercaPersone: FormGroup;
+  constructor(private router:Router, private coreHttp:CoreHttpService){
+    this.ricercaPersone= new FormGroup({
+      nome: new FormControl(""),
+      cognome: new FormControl(""),
+      cf: new FormControl(""),
+      sesso: new FormControl(""),
+      dataNascita: new FormControl("")
+    })
+  }
   ngOnInit(){ 
-    this.getAllPersone()
+    // this.getAllPersone()
   } 
   getAllPersone(){
     this.coreHttp.get<Persone[]>('/getPersone').subscribe((ris: Persone[])=>{
@@ -25,8 +36,29 @@ export class PazienteTriageComponent {
     }
     )
   }
+  getPersoneCercate(){
+    let campiricerca: Partial<RicercaPersona> = this.getValoriCompilati();
+    console.log(campiricerca)
+    this.coreHttp.get<Persone[]>('/ricercaPersone').subscribe((ris: Persone[])=>{
+      this.$persone=ris
+    }
+    )
+  }
+  
   goToDashbord(){
     this.router.navigate(['/'])
+  }
+  getValoriCompilati(): Partial<RicercaPersona>  {
+    const formValues = this.ricercaPersone.value;
+    const campiRiempiti = {};
+  
+    for (const campo in formValues) {
+      if (formValues.hasOwnProperty(campo) && formValues[campo]) {
+        campiRiempiti[campo] = formValues[campo];
+      }
+    }
+  
+    return campiRiempiti;
   }
 
 }
---
+++ src/app/core/interface/ricerca-persona.ts
@@ -0,0 +1,7 @@
+export interface RicercaPersona {
+    nome: string,
+    cognome: string,
+    cf: string,
+    sesso: string,
+    dataNascita: string
+}
